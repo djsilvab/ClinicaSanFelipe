915e59ffb18fe7c5f4fc8663d9097699725f19bf
David Silva <djsilvab@gmail.com>
2026-05-24 12:05:41 -0500

    Añadir navbar con logout y visibilidad según la ruta
    Se implementa un nuevo componente de barra de navegación con enlaces y botón de logout. El navbar se muestra solo si la ruta no es 'login'. Incluye estilos personalizados y pruebas unitarias para el componente.

--- inventory-frontend/src/app/app.component.ts
+++ inventory-frontend/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './shared/layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'inventory-frontend';
+
+  constructor(private router: Router) {}
+
+  showNavbar() {
+    return !this.router.url.includes('login');
+  }
 }
---
+++ inventory-frontend/src/app/shared/layout/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+@Component({
+  selector: "app-navbar",
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: "./navbar.component.html",
+  styleUrl: "./navbar.component.scss",
+})
+export class NavbarComponent {
+  logout() {
+    localStorage.removeItem("token");
+
+    window.location.href = "/login";
+  }
+}
